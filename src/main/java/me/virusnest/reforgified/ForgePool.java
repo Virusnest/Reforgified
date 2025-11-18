30ba7e8bed046fe1072bffc7e01ae75881353f53
AutoUpdate <auto@example.com>
2025-11-18 19:40:25 +1100

    UPDATED REPO

--- src/main/java/me/virusnest/reforgified/ForgePool.java
+++ src/main/java/me/virusnest/reforgified/ForgePool.java
@@ -1,4 +1,12 @@
 package me.virusnest.reforgified;
 
+import net.minecraft.registry.Registry;
+import net.minecraft.util.collection.Pool;
+
+
 public class ForgePool {
+    Pool<ForgePoolEntry> pool;
+    public ForgePool(Registry<ForgePoolEntry> registry) {
+        this.pool = Pool.of(registry.stream().toList());
+    }
 }
